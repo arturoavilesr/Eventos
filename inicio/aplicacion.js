35461291a1e0967d9f7497c523e195cf8fc21851
arturoavilesr <145037428+arturoavilesr@users.noreply.github.com>
2023-11-30 20:54:21 -0600

    Create aplicacion.js
    se crea la otra parte del desplazamiento, su aplicacion

---
+++ inicio/aplicacion.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import ClienteDesplazamiento from './inicio/Desplazamiento';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Aplicación para Bodas</h1>
+      <ClienteDesplazamiento />
+  
+    </div>
+  );
+}
+
+export default aplicacion;
